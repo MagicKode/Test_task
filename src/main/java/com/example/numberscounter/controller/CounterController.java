26485e91a754bddf4b5051ab5fceb01bd8c21cd3
MagicKode <57261227+MagicKode@users.noreply.github.com>
2023-08-09 09:00:47 +0300

    Liquibase added

--- src/main/java/com/example/numberscounter/controller/CounterController.java
+++ src/main/java/com/example/numberscounter/controller/CounterController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path ="/counter")
+@RequestMapping(path ="/counters")
 public class CounterController {
 
     private final CounterService counterService;
